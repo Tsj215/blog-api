f336e544fc7e674d59adc33a452c2b84c6440973
唐司军 <tangsijun@uniontech3d.com>
2020-01-14 17:12:34 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+import { AppModule } from "./appliction/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle("Nli-Blog-Api")
+    .setDescription("The Nli-Blog-Api description")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
